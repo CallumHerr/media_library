78abe1ac5a5769744819e17c25b289d7664584ef
CallumHerr <calherring@outlook.com>
2023-10-24 21:11:57 +0100

    started playlist

--- src/application/PlaylistEditor.java
+++ src/application/PlaylistEditor.java
@@ -1,4 +1,24 @@
 package application;
 
+import util.FileManager;
+
+import javax.swing.*;
+
 public class PlaylistEditor {
+    private final JFrame frame;
+    private final FileManager fileMan;
+
+    public PlaylistEditor(FileManager fileManager) {
+        this.frame = new JFrame();
+        this.fileMan = fileManager;
+
+        JPanel mainPanel = new JPanel();
+        this.frame.setContentPane(mainPanel);
+
+        JTextField nameField = new JTextField();
+        JLabel nameLabel = new JLabel("Playlist name");
+        nameLabel.setLabelFor(nameField);
+
+        mainPanel.add(nameLabel);
+    }
 }
